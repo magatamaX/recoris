6a6ea750e4b2c752cbaafc1fd7816c45e1b8d682
日崎智博 <magatama.club@gmail.com>
2019-07-10 01:30:11 +0900

    「ご利用料金イメージ」と「料金SIM」に修正いただきたい点がございます。 また「DATA METER」の桁数表示に要望がございます。 添付参照願います。

--- src/js/top/sections/_meter.js
+++ src/js/top/sections/_meter.js
@@ -1,3 +1,55 @@
+import { AddClass, RemoveClass } from "mgn-utility";
+
+function Align(num) {
+    if ( num >= 1000000000 && num < 10000000000 ) {
+        return ( num / 1000000 ).toFixed(0)
+    } else if ( num >= 10000000000 && num < 100000000000 ) {
+        return ( num / 10000000000 ).toFixed(3)
+    } else if ( num >= 100000000000 && num < 1000000000000 ) {
+        return ( num / 10000000000 ).toFixed(2)
+    } else if ( num >= 1000000000000 && num < 10000000000000 ) {
+        return ( num / 10000000000 ).toFixed(1)
+    } else if ( num >= 10000000000000 ) {
+        return ( num / 10000000000 ).toFixed(0)
+    }
+};
+
+function CountUp( target, num, callback ) {
+
+    var time = 2000;
+    var START_TIME = new Date().getTime();　//描画開始時刻を取得
+
+    var Loop = setInterval( function(){
+
+        var CURRENT_TIME = new Date().getTime(); //経過時刻を取得
+        var STATUS = CURRENT_TIME - START_TIME; // 描画開始時刻から経過時刻を引く
+
+        var val = easeOutQuint(STATUS, 0, num, time);
+            val = Math.round(val);
+            val = String(val).split(""); //1の位と10の位に分割
+
+        for (var i = 0; i < val.length; i++) {
+            var place = (target.length - val.length) + i;
+            target[place].style.opacity = 1;
+            val[i] == 1 ? AddClass( target[place], "one" ) : RemoveClass( target[place], "one" );
+            target[place].innerHTML = val[i];
+        }
+
+        if( STATUS >= time ) {
+            clearInterval( Loop );
+            if(callback) callback();
+        }
+
+    }, 33);
+
+    function easeOutQuint(t, b, c, d) {
+        t /= d;
+        t = t - 1;
+        return c*(t*t*t + 1) + b;
+    }
+
+}
+
 const Meter = () => {
 
     // console.log("/top/sections/_meter.js");
@@ -14,11 +66,11 @@ const Meter = () => {
 
         fetch('https://app.recoris.jp/api/total-usage')
             .then( response => response.json())
-            .then( response => {               
+            .then( response => {
                 HOUR.innerText = response.time.split(":")[0];
                 MINUTE.innerText = response.time.split(":")[1];
                 SECOND.innerText = response.time.split(":")[2];
-                MB.innerText = String(Math.round(response.size / 1048576)).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+                MB.innerText = String(Align(response.size)).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
             })
             .catch( error => {
                 console.log("取得できません。", error)

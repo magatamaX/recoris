1f10e27b077476946adac5310f20e8fc31aba0d6
Tomohiro Hizaki <magatama.club@gmail.com>
2019-07-12 14:18:03 +0900

    3.RECORiSアニメーションの表示が遅いため、軽いもの（添付）に差し替えをお願いいたします。 4.弊社サイトのようにRECORiSアニメーションの右隅に音声のオンオフボタンをつけてください。

--- src/js/top/sections/_kv.js
+++ src/js/top/sections/_kv.js
@@ -1,6 +1,22 @@
 const Kv = () => {
 
     // console.log("/top/sections/_kv.js");
+    if ( !document.getElementById('kvVideo')) {
+        return;
+    }
+
+    const VIDEO = document.getElementById('kvVideo').querySelector('video');
+    const BUTTON = document.getElementById('kvVideo').querySelector('button');
+
+    BUTTON.addEventListener('click', function () {
+        if(VIDEO.muted){
+            VIDEO.muted = false;
+            BUTTON.className = "on";
+        }else{
+            VIDEO.muted = true;
+            BUTTON.className = "off";
+        }
+    });
 
 };
 

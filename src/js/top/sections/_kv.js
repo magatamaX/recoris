71006acafc81a25aad31fe24f56ab81a349ab8dc
日崎智博 <magatama.club@gmail.com>
2019-07-21 02:27:55 +0900

    iOSでうまくうごかないで調整

--- src/js/top/sections/_kv.js
+++ src/js/top/sections/_kv.js
@@ -1,3 +1,5 @@
+import mgnUa from "mgn-ua";
+
 const Kv = () => {
 
     // console.log("/top/sections/_kv.js");
@@ -6,32 +8,68 @@ const Kv = () => {
     }
 
     const VIDEO = document.getElementById('kvVideo').querySelector('video');
+    const SRC = "/assets/video/video.mp4?v=20190721";
     const BUTTON = document.getElementById('kvVideo').querySelector('button');
     const LOADING_ICON = document.getElementById('kvVideo').querySelector('.loading');
 
-    VIDEO.addEventListener("loadeddata", e => {
-        ( async ()=> {
+    const Ua = new mgnUa();
 
-            const Sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+    const PlayVideo = async () => {
+        const Sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+        await Sleep(700);
+        LOADING_ICON.className = await "loading loaded";
+        await Sleep(500);
+        LOADING_ICON.className = await "loading loaded off";
+    }
 
+    if ( Ua.isSp ) {
+
+        // console.log("SP");
+        const VIDEO_HOLDER = document.querySelector('.p-kv_video_element');
+        VIDEO_HOLDER.innerHTML = "";
+
+        const NEW_VIDEO = document.createElement('video');
+        VIDEO_HOLDER.appendChild(NEW_VIDEO);
+        
+        NEW_VIDEO.muted = true;
+        NEW_VIDEO.setAttribute("playsinline", "");
+        NEW_VIDEO.setAttribute("webkit-playsinline", "");
+        NEW_VIDEO.autoplay = true;
+        NEW_VIDEO.addEventListener("play", () => {
+            PlayVideo();
+        }, false);
+        NEW_VIDEO.src = VIDEO_HOLDER.dataset.src;
+
+        BUTTON.addEventListener('click', function () {
+            if(NEW_VIDEO.muted){
+                NEW_VIDEO.muted = false;
+                BUTTON.className = "on";
+            }else{
+                NEW_VIDEO.muted = true;
+                BUTTON.className = "off";
+            }
+        });
+
+    } else {
+
+        // console.log("Not SP");
+
+        VIDEO.addEventListener("loadeddata", () => {
             VIDEO.play();
-            await Sleep(700);
-            LOADING_ICON.className = await "loading loaded";
-            await Sleep(500);
-            LOADING_ICON.className = await "loading loaded off";
+            PlayVideo();
+        }, false);
 
-        })();
-    })
+        BUTTON.addEventListener('click', function () {
+            if(VIDEO.muted){
+                VIDEO.muted = false;
+                BUTTON.className = "on";
+            }else{
+                VIDEO.muted = true;
+                BUTTON.className = "off";
+            }
+        });
 
-    BUTTON.addEventListener('click', function () {
-        if(VIDEO.muted){
-            VIDEO.muted = false;
-            BUTTON.className = "on";
-        }else{
-            VIDEO.muted = true;
-            BUTTON.className = "off";
-        }
-    });
+    }
 
 };
 
